0727e07e88ede8a49b0fa835a1477e751042da21
Vincent BESANCON <besancon.vincent@gmail.com>
2016-01-12 02:24:14 +0100

    [core] create an abstract model for storing phone numbers.

--- core/models.py
+++ core/models.py
@@ -20,5 +20,20 @@
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+
+class AbstractPhoneNumber(models.Model):
+    """Abstract model for storing a labelized phone number."""
+    label = models.CharField(max_length=32, verbose_name=_('phone_label_field'))
+    # Translators: this is the phone number here.
+    number = PhoneNumberField(verbose_name=_('phone_number_field'))
+
+    class Meta:
+        abstract = True
+        verbose_name = _('phone number')
+        verbose_name_plural = _('phone numbers')
+
+    def __str__(self):
+        return '{0.number} ({0.label})'.format(self)
