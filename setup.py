ed50d58ee588231d40b1b9ae0f264e73a91eaa6f
Vincent BESANCON <besancon.vincent@gmail.com>
2016-01-16 18:29:30 +0100

    Base setup.py file.
    Come back later :-)

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mystartupmanager',
+    version='0.0.1',
+    packages=find_packages(),
+    url='https://github.com/nVentiveUX/mystartupmanager',
+    license='MIT',
+    author='nVentiveUX',
+    # TODO: we need a team email here ! ;-)
+    author_email='besancon.vincent@gmail.com',
+    description='A Django project to help new entrepreneurs',
+    # TODO: fetch dependencies from requirements
+)
